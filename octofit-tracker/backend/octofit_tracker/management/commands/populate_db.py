77006bb180ebcf2e7c2c50495800c668cf60598f
Valeria Daukshis <47060604+ValeriaDaukshis@users.noreply.github.com>
2026-04-20 15:09:35 +0000

    Add management commands to populate octofit_db with test data

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        # Clear existing data
+        Activity.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Workout.objects.all().delete()
+        Leaderboard.objects.all().delete()
+
+        # Create Teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create Users
+        tony = User.objects.create(name='Tony Stark', email='tony@stark.com', team=marvel)
+        steve = User.objects.create(name='Steve Rogers', email='steve@rogers.com', team=marvel)
+        bruce = User.objects.create(name='Bruce Wayne', email='bruce@wayne.com', team=dc)
+        clark = User.objects.create(name='Clark Kent', email='clark@kent.com', team=dc)
+
+        # Create Activities
+        Activity.objects.create(user=tony, type='Running', duration=30, date=timezone.now())
+        Activity.objects.create(user=steve, type='Cycling', duration=45, date=timezone.now())
+        Activity.objects.create(user=bruce, type='Swimming', duration=60, date=timezone.now())
+        Activity.objects.create(user=clark, type='Yoga', duration=20, date=timezone.now())
+
+        # Create Workouts
+        w1 = Workout.objects.create(name='Super Strength', description='Strength workout for superheroes')
+        w2 = Workout.objects.create(name='Flight Training', description='Aerobic workout for flying heroes')
+        w1.suggested_for.add(marvel, dc)
+        w2.suggested_for.add(dc)
+
+        # Create Leaderboards
+        Leaderboard.objects.create(team=marvel, points=100)
+        Leaderboard.objects.create(team=dc, points=120)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data!'))
